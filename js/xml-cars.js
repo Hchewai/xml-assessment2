ba5860b49692ac07e2a0bfd4862c14ce5f9e6fc4
Hassan Chewai <201274422@tafe.wa.edu.au>
2026-08-19 08:50:32 +0800

    added vehicle article variables in js file

---
+++ js/xml-cars.js
@@ -0,0 +1,7 @@
+const vehicleArticle = document.getElementById("vehicle");
+const xmlButton = document.getElementById("loadXML");
+const modelDisplay = document.getElementById("model");
+const countryDisplay = document.getElementById("country");
+const companyDisplay = document.getElementById("company");
+const priceDisplay = document.getElementById("price");
+const yearDisplay = document.getElementById("year");

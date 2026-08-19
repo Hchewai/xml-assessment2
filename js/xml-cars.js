e419d5064d75cc4a52ec0be26feef45bbbe998a0
Hchewai <20127442@tafe.wa.edu.au>
2026-08-19 17:14:04 +0800

    added a empty error message in javascript for the load cars button
    edited schema to fix redundant lines by xmlspy generator
    
    removed xs:import and deleted unnecessary carschema1 file

--- js/xml-cars.js
+++ js/xml-cars.js
@@ -5,3 +5,31 @@ const countryDisplay = document.getElementById("country");
 const companyDisplay = document.getElementById("company");
 const priceDisplay = document.getElementById("price");
 const yearDisplay = document.getElementById("year");
+
+// function loadModel() {
+//   xmlButton.addEventListener("click", () => {
+//     let newModelDisplay = (document.getElementById("model").textContent =
+//       "test");
+//     console.log(newModelDisplay);
+//   });
+// }
+// Some javascript practice
+function loadModel() {
+  xmlButton.addEventListener("click", () => {
+    if (modelDisplay.textContent === "") {
+      let newModelDisplay = (document.getElementById("model").textContent =
+        "Sorry we dont have anything to display at the moment, please check back later!");
+      const failImage = document.createElement("img");
+      failImage.src = "/images/broken_heart.png";
+      failImage.height = 100;
+      failImage.width = 100;
+      failImage.style.margin = "0 auto";
+      const article = document.querySelector("article");
+      article.append(failImage);
+    }
+  });
+}
+
+loadModel();
+
+function xmlRequest() {}

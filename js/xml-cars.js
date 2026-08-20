641a6a9f2c87a8fa2aee57a629d4adbc6f3ffb12
Hassan Chewai <201274422@tafe.wa.edu.au>
2026-08-20 08:34:11 +0800

    used xml object request from class but doesnt do it how i want to do it

--- js/xml-cars.js
+++ js/xml-cars.js
@@ -15,38 +15,37 @@ const xmlButton = document.getElementById("loadXML");
 // display empty message
 //remember to not check text.content === "", check if xml is empty
 // i might be goated
-function emptyMessage() {
-  xmlButton.addEventListener("click", () => {
-    if (vehicleList.textContent === "") {
-      let apology = (document.getElementById("vehicle-list").textContent =
-        "sorry we dont have anything to display, please come back later");
-      const failImage = document.createElement("img");
-      failImage.src = "/images/broken_heart.png";
-      failImage.height = 100;
-      failImage.width = 100;
-      failImage.style.margin = "0 auto";
-      const container = document.getElementById("vehicle-catalogue");
-      container.append(failImage);
+
+function loadXMLContent() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status === 200) {
+      document.getElementById("vehicle-list").innerHTML = this.responseText;
     }
-  });
+  };
+  xhttp.open("GET", "resources/cars.xml", true);
+  xhttp.send();
 }
 
-emptyMessage();
+function init() {
+  document.getElementById("loadXML").addEventListener("click", loadXMLContent);
+}
 
-// function xmlRequest() {}
+loadXMLContent();
+// function emptyMessage() {
+//   xmlButton.addEventListener("click", () => {
+//     if (vehicleList.textContent === "") {
+//       let apology = (document.getElementById("vehicle-list").textContent =
+//         "sorry we dont have anything to display, please come back later");
+//       const failImage = document.createElement("img");
+//       failImage.src = "/images/broken_heart.png";
+//       failImage.height = 100;
+//       failImage.width = 100;
+//       failImage.style.margin = "0 auto";
+//       const container = document.getElementById("vehicle-catalogue");
+//       container.append(failImage);
+//     }
+//   });
+// }
 
-// get javascript to retrieve my xml data
-
-// then get javascript to create articles for each individual xml vehicle tag
-
-// let newVehicleDisplay = (document.getElementById(
-//   "vehicle-list",
-// ).textContent =
-//   "Sorry we dont have anything to display at the moment, please come back later!");
-// const failImage = document.createElement("img");
-// failImage.src = "/images/broken_heart.png";
-// failImage.height = 100;
-// failImage.width = 100;
-// failImage.style.margin = "0 auto";
-// const vehicleList = document.getElementById("vehicle-list");
-// div.append(failImage);
+// emptyMessage();

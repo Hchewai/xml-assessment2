1b91f162147f635aedc2a9a4b80daebd4365c479
Hchewai <20127442@tafe.wa.edu.au>
2026-08-20 20:16:45 +0800

    XML renders correctly by assessment standards
    added use strict
    
    used a small template literal for the xml content

--- js/xml-cars.js
+++ js/xml-cars.js
@@ -4,10 +4,12 @@ const xmlButton = document.getElementById("loadXML");
 //pseudo-code
 //when xmlhttp request is rendered, apply the classListName of article to XML content to give the html output the correct styling
 function loadXMLContent() {
+  "use strict";
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status === 200) {
-      document.getElementById("vehicle-list").innerHTML = this.responseText;
+      document.getElementById("vehicle-list").innerHTML =
+        `<article>${this.responseText}</article>`;
     }
   };
   xhttp.open("GET", "resources/cars.xml", true);
@@ -15,27 +17,8 @@ function loadXMLContent() {
 }
 
 function init() {
+  "use strict";
   document.getElementById("loadXML").addEventListener("click", loadXMLContent);
 }
 
-loadXMLContent();
-
-// Some javascript practice
-
-// function emptyMessage() {
-//   xmlButton.addEventListener("click", () => {
-//     if (vehicleList.textContent === "") {
-//       let apology = (document.getElementById("vehicle-list").textContent =
-//         "sorry we dont have anything to display, please come back later");
-//       const failImage = document.createElement("img");
-//       failImage.src = "/images/broken_heart.png";
-//       failImage.height = 100;
-//       failImage.width = 100;
-//       failImage.style.margin = "0 auto";
-//       const container = document.getElementById("vehicle-catalogue");
-//       container.append(failImage);
-//     }
-//   });
-// }
-
-// emptyMessage();
+window.onload = init;

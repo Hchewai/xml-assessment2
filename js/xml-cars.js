0635c1aa44e386367b94ecbe83eef05024fd4ccd
Hassan Chewai <201274422@tafe.wa.edu.au>
2026-08-26 13:53:56 +0800

    added code to loop through XML text content by tag name and put it into a table
    changed image name to error-image.png, got error conditional to work

--- js/xml-cars.js
+++ js/xml-cars.js
@@ -1,21 +1,56 @@
-const vehicleList = document.getElementById("vehicle-list");
-const xmlButton = document.getElementById("loadXML");
+//Formatting XML Content (used text content instead of w3schools child nodes for readability)
+//https://www.w3schools.com/xml/tryit.asp?filename=try_dom_xmlhttprequest_xml
+function formatXML(xml) {
+  let x, i, xmlDoc, table;
+  xmlDoc = xml.responseXML;
+  table =
+    "<tr><th>Vehicle Name</th><th>Model</th><th>Country</th><th>Company</th><th>Price</th><th>Year</th></tr>";
+  x = xmlDoc.getElementsByTagName("vehicle");
+  for (i = 0; i < x.length; i++) {
+    table +=
+      "<tr><td>" +
+      x[i].getElementsByTagName("name")[0].textContent +
+      "</td><td>" +
+      x[i].getElementsByTagName("model")[0].textContent +
+      "</td><td>" +
+      x[i].getElementsByTagName("country")[0].textContent +
+      "</td><td>" +
+      x[i].getElementsByTagName("company")[0].textContent +
+      "</td><td>" +
+      x[i].getElementsByTagName("price")[0].textContent +
+      "</td><td>" +
+      x[i].getElementsByTagName("year")[0].textContent +
+      "</td></tr>";
+  }
+  document.getElementById("vehicle-list").innerHTML = table;
+}
 
-//pseudo-code
-//when xmlhttp request is rendered, apply the classListName of article to XML content to give the html output the correct styling
+// Load XML Content Function
 function loadXMLContent() {
   "use strict";
-  var xhttp = new XMLHttpRequest();
+  let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status === 200) {
-      document.getElementById("vehicle-list").innerHTML =
-        `<article>${this.responseText}</article>`;
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        formatXML(this);
+      } else {
+        let apology = (document.getElementById("vehicle-list").textContent =
+          "sorry we dont have anything to display, please come back later");
+        const failImage = document.createElement("img");
+        failImage.src = "/images/error-image.png";
+        failImage.height = 100;
+        failImage.width = 100;
+        failImage.style.margin = "0 auto";
+        const container = document.getElementById("vehicle-catalogue");
+        container.append(failImage); // if you'd like to see this, change line 49's cars.xml to a different nonexistent name - Hassan
+      }
     }
   };
   xhttp.open("GET", "resources/cars.xml", true);
   xhttp.send();
 }
 
+// // Event Listener
 function init() {
   "use strict";
   document.getElementById("loadXML").addEventListener("click", loadXMLContent);
